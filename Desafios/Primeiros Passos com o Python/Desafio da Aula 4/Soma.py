1cf91f2f2936e4c446733979b1091e87a39c98ab
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-05 16:51:42 -0300

    Add files via upload

---
+++ Desafios/Primeiros Passos com o Python/Desafio da Aula 4/Soma.py
@@ -0,0 +1,3 @@
+primeiro=int(input('Digite o primeiro número:'))
+segundo=int(input('Digite o segundo numero:'))
+print('E a soma e' + str (primeiro + segundo))
---
+++ Scripts-python/teste01.py
@@ -0,0 +1,11 @@
+print('Olá, Mundo!')
+print(7+4)
+print('7'+'4')
+nome = 'Guanabara'
+idade = 25
+peso = 75.8
+print(nome, idade, peso)
+nome = input('Qual é o seu nome?')
+idade = input('Qual e a sua idade?')
+peso = input('Qual e o seu peso?')
+print(nome, idade, peso)
