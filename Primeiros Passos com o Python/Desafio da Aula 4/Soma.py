4e6e6733979a4eba27b451a42a8e7e27121a7d88
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-24 11:03:35 -0300

    Delete Primeiros Passos com o Python/Desafio da Aula 4 directory

--- Primeiros Passos com o Python/Desafio da Aula 4/Soma.py
+++
@@ -1,3 +0,0 @@
-primeiro=int(input('Digite o primeiro número:'))
-segundo=int(input('Digite o segundo numero:'))
-print('E a soma e' + str (primeiro + segundo))
--- Primeiros Passos com o Python/Desafio da Aula 4/datadenascimento.py
+++
@@ -1,4 +0,0 @@
-dia = input('Qual e o dia do seu nascimento? ')
-mês = input('Qual e o mês do seu nascimento? ')
-ano = input('Qual e o ano do seu nascimento? ')
-print('Você nasceu no dia ' + dia, 'do mês ' + mês, 'no ano de ' + ano, '.Está Correto?')
