471bddf340c1b9cd9fe439a6c899fbaf793f6909
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-25 11:44:18 -0300

    Add files via upload

--- Desafios/Aula 7/desafio05.py
+++ Desafios/Aula 7/desafio05.py
@@ -1,1 +1,5 @@
 #Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+n1 = int(input('Digite um número: '))
+suc = n1 + 1
+ant = n1 - 1
+print('O número {} tem o seu sucessor {} e o seu antecessor {}'.format(n1, suc, ant))
