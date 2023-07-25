4454157286b4864cbceb53206195b53a82266b6d
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-25 18:04:06 -0300

    Add files via upload

--- Desafios/Aula 7/desafio06.py
+++ Desafios/Aula 7/desafio06.py
@@ -1,1 +1,7 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+n1 = int(input('Digite um número: '))
+dob = n1 * 2
+tri = n1 * 3
+qua = n1 ** (1/2)
+print('O número {} tem o dobro de {} \nCom um triplo de {} \nCom uma raiz quadrada de {:.3f}'.format(n1, dob, tri, qua))
+
