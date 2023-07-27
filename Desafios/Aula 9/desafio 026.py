cffc30799d0a01aa874b802a0ac2516bc51b7787
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-27 18:36:10 -0300

    Add files via upload

--- Desafios/Aula 9/desafio 026.py
+++ Desafios/Aula 9/desafio 026.py
@@ -1,6 +1,10 @@
 '''
-Faça um programa que leia ma frase pelo teclado e mostre:
+Faça um programa que leia na frase pelo teclado e mostre:
 ->Quantas vezes aparece a letra "A".
 ->Em que posição ela aparece a primeira vez.
 ->Em que posição ela aparece a úlltima vez.
 '''
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra "A" apareceu {} vezes.'.format(frase.count('A')))
+print('A primeira letra "A" aparaceu na posição {}'.format(frase.find('A') + 1))
+print('A ultima letra "A" apareceu na posição {}'.format(frase.rfind('A') + 1))
