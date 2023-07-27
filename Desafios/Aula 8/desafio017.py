07d8a48c5777d8989b31064a08e5d65511038d03
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-27 18:35:52 -0300

    Add files via upload

--- Desafios/Aula 8/desafio017.py
+++ Desafios/Aula 8/desafio017.py
@@ -1,1 +1,18 @@
-#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triãngulo retângulo, calcule e mostre o comprimento da hipotenusa.
+'''Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triãngulo retângulo, calcule e mostre o comprimento da hipotenusa.'''
+
+'''co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+h1 = (co ** 2 + ca ** 2) ** (1/2)
+print('A hipotenusa vai medir {:.2f}'.format(h1))'''
+
+'''import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimetno do cateto adjacente: '))
+h1 = math.hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(h1))'''
+
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+h1 = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(h1))
