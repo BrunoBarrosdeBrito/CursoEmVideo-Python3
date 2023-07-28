50b365853c3b8ec94d1632b252b59990fa0422f1
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-28 11:44:29 -0300

    Add files via upload

--- Desafios/Aula 10/desafio 29.py
+++ Desafios/Aula 10/desafio 29.py
@@ -3,3 +3,8 @@ Escreva um programa que leia a velocidade de um carro.
 Se ele ultrapassar 80Km/h, mostra uma mensagem dizendo que ele foi multado.
 A multa vai R$7,00 por cada Km acima do limite.
 '''
+velocidade = int(input('Em qual velocidade você estava? '))
+if velocidade >80:
+    print('Você foi multado por excesso de velocidade que é de 80Km/h, o valor de multa é R${:.2f}.'.format((velocidade - 80) * 7))
+else:
+    print('Você estava na velocidade recomendada, então não havera multa.')
