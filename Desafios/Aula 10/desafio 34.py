edd35d7595f5b12bf4ba065dfc151e1943dcf3c1
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-28 10:39:58 -0300

    Delete Desafios directory

--- Desafios/Aula 10/desafio 34.py
+++
@@ -1,5 +0,0 @@
-'''
-Escreva um programa que pergunte o salário de um funcionário e calcule o valor de seu aumento.
-Para salário superiores a R$1.250,00, calcule um aumento de 10%.
-Para os inferiores ou iguais o aumento é de 15%.
-'''
--- Desafios/Aula 7/desafio08.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milímetros.
-n1 = float(input('Digite aqui a quantidade de metros: '))
-cm = n1 * 100
-mm = n1 * 1000
-print('A quantidade de metros foi {} \nEm cm é {:.0f} \nEm mm é {:.0f}'.format(n1, cm, mm))
