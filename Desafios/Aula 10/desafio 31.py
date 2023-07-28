5a53f581f791b6fb227779555e85a2dbdedcd4c8
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-28 17:19:32 -0300

    Add files via upload

--- Desafios/Aula 10/desafio 31.py
+++ Desafios/Aula 10/desafio 31.py
@@ -1,4 +1,9 @@
 '''
 Desenvolva um programa que pergunte a distância de uma viagem em Km.
-CalculeO preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
+Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
 '''
+km = int(input('Qual e a distancia da viagem? '))
+if km <=200:
+    print('O preço da passagem é R${:.2f}'.format(km * 0.50))
+else:
+    print('O preço da passagem é R${:.2f}'.format(km * 0.45))
