74d6434e600d3eb5a202f2b37bbad2f1887fada3
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-28 18:47:12 -0300

    Add files via upload

--- Desafios/Aula 10/desafio 34.py
+++ Desafios/Aula 10/desafio 34.py
@@ -3,3 +3,9 @@ Escreva um programa que pergunte o salário de um funcionário e calcule o valor
 Para salário superiores a R$1.250,00, calcule um aumento de 10%.
 Para os inferiores ou iguais o aumento é de 15%.
 '''
+salário = float(input('Digite o salário do funcionario: R$'))
+if salário <=1250:
+    novo = salário + (salário * 15 / 100)
+else:
+    novo = salário + (salário * 10 / 100)
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salário, novo))
