75bd97baef770db82c4bd9d31afae44f58a77392
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-28 18:02:08 -0300

    Add files via upload

--- Desafios/Aula 10/desafio 32.py
+++ Desafios/Aula 10/desafio 32.py
@@ -1,3 +1,8 @@
 '''
 Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
 '''
+ano = int(input('Que ano quer aalisar? Coloque 0 analise o ano atual: '))
+if ano % 4 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} não e BISSEXTO'.format(ano))
