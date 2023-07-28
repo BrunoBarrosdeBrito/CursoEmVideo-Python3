1e85f38fed0864c4c469f397bdb5588395c7de2e
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-28 10:38:49 -0300

    Add files via upload

---
+++ Desafios/Aula 10/desafio 29.py
@@ -0,0 +1,5 @@
+'''
+Escreva um programa que leia a velocidade de um carro.
+Se ele ultrapassar 80Km/h, mostra uma mensagem dizendo que ele foi multado.
+A multa vai R$7,00 por cada Km acima do limite.
+'''
