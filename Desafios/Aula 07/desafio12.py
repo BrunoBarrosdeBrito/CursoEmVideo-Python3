df7b12599e12006ee3f465732e7579a86cdee538
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-31 10:42:44 -0300

    Update desafio12.py

--- Desafios/Aula 07/desafio12.py
+++ Desafios/Aula 07/desafio12.py
@@ -1,4 +1,4 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 prod = float(input('Qual é o preço do produto? R$'))
 novo = prod - (prod * 5 / 100) 
-print('O produto que custava {:.2f}, na promoção com o desconto de 5% vai cistar {:.2f}.'.format(prod, novo))
+print('O produto que custava {:.2f}, na promoção com o desconto de 5% vai custar {:.2f}.'.format(prod, novo))
