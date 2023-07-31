21f6a5a151c9c546f8623b4968280dac051cc6ac
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-07-31 12:33:15 -0300

    Add files via upload

--- Desafios/Aula 09/desafio 027.py
+++ Desafios/Aula 09/desafio 027.py
@@ -4,3 +4,7 @@ EX: Ana Maria de Souza
 primeiro= Ana
 ultimo= Souza
 '''
+nome = str(input('Digite o seu nome completo: ')).strip()
+separar = nome.split()
+print('O seu primeiro nome é {}'.format(separar[0]))
+print('O seu último nome é {}'.format(separar[len(nome) - 1]))
