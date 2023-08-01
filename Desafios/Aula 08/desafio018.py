3271d59588f339003651e64133fffa61523256ed
Bruno Barros <137173727+BrunoBarrosdeBrito@users.noreply.github.com>
2023-08-01 09:04:43 -0300

    Update desafio018.py

--- Desafios/Aula 08/desafio018.py
+++ Desafios/Aula 08/desafio018.py
@@ -1,4 +1,4 @@
-#Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e angente desse ângullo.
+#Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângullo.
 import math
 ang = float(input('Digite o ângulo que você deseja: '))
 seno = math.sin(math.radians(ang))
